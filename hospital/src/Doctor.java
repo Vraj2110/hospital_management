a155bdc53a1b930fc2aa40e61df2e9de63234771
Vraj Desai <desaivraj18@gmail.com>
2026-06-04 22:37:50 +0530

    Doctor class are done next will be a Appoitment class

--- hospital/src/Doctor.java
+++ hospital/src/Doctor.java
@@ -1,2 +1,23 @@
-public class Doctor {
+public class Doctor extends Person {
+    private String specialization;
+
+    public Doctor(String name, int id, String specialization) {
+        super(id, name);
+        setSpecialization(specialization);
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        if (specialization != null && !specialization.isEmpty()) {
+            this.specialization = specialization;
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Doctor name ="+ getName()+"Doctor id ="+ getId()+"Doctor specialization ="+ specialization);
+    }
 }
