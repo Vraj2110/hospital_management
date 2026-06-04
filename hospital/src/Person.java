a43e33fc6034ef86406904d5b42917b032b24788
Vraj Desai <desaivraj18@gmail.com>
2026-06-04 22:03:59 +0530

    person are done next is  patient class

--- hospital/src/Person.java
+++ hospital/src/Person.java
@@ -1,2 +1,24 @@
-public class Person {
+public abstract class Person implements Displayable {
+    private int id;
+    private String name;
+
+    public Person(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null && name.isEmpty())
+        this.name = name;
+    }
+
+    public abstract void Displayable();
 }
