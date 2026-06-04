4f21575fee548193fb8655cf6d34835e1bbf0df2
Vraj Desai <desaivraj18@gmail.com>
2026-06-04 22:43:46 +0530

    Appoitment done nxt will be hoshpitalmanager

--- hospital/src/Appoitment.java
+++ hospital/src/Appoitment.java
@@ -1,2 +1,15 @@
 public class Appoitment {
+    private Patient patient;
+    private Doctor doctor;
+
+    public Appoitment(Patient patient,Doctor doctor){
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public void display(){
+        System.out.println("\\n--- Appointment Details ---");
+        patient.display();
+        doctor.display();
+    }
 }
