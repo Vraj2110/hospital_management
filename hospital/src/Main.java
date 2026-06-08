69127ee767f541d1c3825355a9eed93b1b649d3d
Vraj Desai <desaivraj18@gmail.com>
2026-06-08 22:53:55 +0530

    completed a one more mini java project

--- hospital/src/Main.java
+++ hospital/src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Hospitalmanager hm = new Hospitalmanager();
+
+        while (true) {
+            System.out.println("\n1. Add Patient");
+            System.out.println("2. Add Doctor");
+            System.out.println("3. Book Appointment");
+            System.out.println("4. View Appointments");
+            System.out.println("5. Exit");
+
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter ID Name Age: ");
+                    hm.addPatient(new Patient(sc.nextLine(), sc.nextInt(), sc.nextInt()));
+                    break;
+
+                case 2:
+                    System.out.println("Enter ID Name Specialization: ");
+                    hm.addDoctor(new Doctor(sc.next(), sc.nextInt(), sc.next()));
+                    break;
+
+                case 3:
+                    System.out.println("Enter Patient ID & Doctor ID: ");
+                    hm.bookAppoitment(sc.nextInt(), sc.nextInt());
+                    break;
+
+                case 4:
+                    hm.showAll();
+                    break;
+
+                case 5:
+                    return;
+            }
+        }
     }
 }
